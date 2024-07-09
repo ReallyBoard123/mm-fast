db6f6106ddfa215361795f3ac25bb21be0c37cc5
ReallyBoard123 <chirag.prakriya@gmail.com>
2024-07-09 12:50:25 +0200

    python init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  output: 'export', 
+  async rewrites() {
     return [
       {
         source: "/api/:path*",
@@ -27,4 +28,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
