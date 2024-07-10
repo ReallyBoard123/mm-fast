64accb25f7c75c0928d8c8b3aa36415abe23314d
ReallyBoard123 <chirag.prakriya@gmail.com>
2024-07-10 14:26:42 +0200

    chore: Update .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NODE_ENV === "development"
-          ? "http://127.0.0.1:8000/api/:path*"
-          : "https://your-backend-url.onrender.com/api/:path*",  // Update this with your Render backend URL
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
-      
     ];
   },
 };
