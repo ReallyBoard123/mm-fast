62ebfcb54829ec66651462dab46e01c4b18b4989
ReallyBoard123 <chirag.prakriya@gmail.com>
2024-07-11 09:42:10 +0200

    try deployment

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
