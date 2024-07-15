6f1e2934274bf1df9bc5d09a8a6d5727104483ce
ReallyBoard123 <chirag.prakriya@gmail.com>
2024-07-15 10:23:21 +0200

    chore: Update allow_origins in API middleware configuration

--- app/components/APIForm.tsx
+++ app/components/APIForm.tsx
@@ -55,7 +55,7 @@ export function APIForm() {
     } catch (error) {
       console.error("Error:", error)
       if (axios.isAxiosError(error)) {
-        setLogs((prevLogs) => [...prevLogs, (error as Error).message])
+        setLogs((prevLogs) => [...prevLogs, error.message])
       } else {
         setLogs((prevLogs) => [...prevLogs, "An unknown error occurred"])
       }
@@ -63,7 +63,6 @@ export function APIForm() {
       setLoading(false)
     }
   }
-  
 
   return (
     <div className="space-y-8">
